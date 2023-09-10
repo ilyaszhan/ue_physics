287153515383f18fbfeb27fcc78177e4a7f8fee0
Zhan Ilyas <ilyasov.zhan@gmail.com>
2023-09-11 05:09:38 +0600

    first commit

---
+++ Source/PhysicsTest/PhysicsTest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhysicsTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhysicsTest, "PhysicsTest" );
+ 
